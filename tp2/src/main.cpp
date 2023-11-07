3208bb95c8200b51d2d63db1ebbe117387f79c28
NicolasSt-Arnault <89414403+NicolasSt-Arnault@users.noreply.github.com>
2023-11-07 10:34:28 -0500

    Gérer la température.

--- tp2/src/main.cpp
+++ tp2/src/main.cpp
@@ -37,11 +37,35 @@ void HandleFileRequest(){
   else {
     httpd.send(404,"text/plain", "404 not found");
   }
-
 }
+
+const int thermometre = A0;
+const int activationRelais = D1;
+
+double temperatureCourante;
+
+String tempsTemperatureStable = "";
+
+int max2Minutes = 0;
+int min2Minutes = 0;
+
+int max5Minutes = 0;
+int min5Minutes = 0;
+
+int maxActuel = 0;
+int minActuel = 0;
+
+bool arretTotal = false;
+
+unsigned long tempsAvantTemperatureStable = 0;
+unsigned long tempsEcoule2DernieresMinutes = 0;
+unsigned long tempsEcoule5DernieresMinutes = 0;
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(activationRelais, OUTPUT);
+
   Serial.println("Creation de l'AP");
               
   WiFi.softAP("TravailPratique2-w", "motdepasse");
@@ -53,7 +77,72 @@ void setup() {
 
 }
 
+void CalculerTempsTemperatureStable(){
+    unsigned int secondes = (millis() - tempsAvantTemperatureStable) / 1000; 
+  	
+    unsigned int minutes = secondes / 60;
+    
+    unsigned int heures = minutes / 60;
+  	  
+    heures %= 24;
+  	minutes %= 60;
+    secondes %= 60;
+
+    tempsTemperatureStable = String(heures) + ":" + String(minutes) + ":" + String(secondes);
+}
+
+void CalculerTemperature(){
+  int valeurTemperature = analogRead(thermometre);
+
+  double voltage = valeurTemperature * 5.0 / 1023.0;
+  double resistance = 10000.0 * voltage / (5.0 - voltage);
+  double tempKelvin = 1.0 / (1.0 / 298.15 + log(resistance / 10000.0) / 3977); //Beta = 3977
+
+  temperatureCourante = tempKelvin - 273.15;
+}
+
 void loop() {
- httpd.handleClient();
+  if(arretTotal != true){
+
+    CalculerTemperature();
+
+    if(temperatureCourante >= 50){
+      arretTotal = true;
+      digitalWrite(activationRelais, LOW);
+    }
+    else{
+      
+      if(temperatureCourante > 41 && temperatureCourante < 45){
+        CalculerTempsTemperatureStable();
+      }
+      else{
+        tempsTemperatureStable = "";
+        tempsAvantTemperatureStable = millis();
+      }
+
+
+      if(minActuel > temperatureCourante)
+        minActuel = temperatureCourante;
+
+      if(maxActuel < temperatureCourante)
+        maxActuel = temperatureCourante;
+
+      if (((millis() - tempsEcoule2DernieresMinutes) / 1000) / 60 > 2)
+      {
+        tempsEcoule2DernieresMinutes = millis();
+        min2Minutes = minActuel;
+        max2Minutes = maxActuel;
+      }
+  
+      if (((millis() - tempsEcoule5DernieresMinutes) / 1000) / 60 > 5)
+      {
+        tempsEcoule5DernieresMinutes = millis();
+        min5Minutes = minActuel;
+        max5Minutes = maxActuel;
+      }
+
+      httpd.handleClient();
+    }
+  }
 }
 
